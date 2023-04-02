80df285ba7e5874c77d4400a4faa4f990363909b
Sumanth Yeodti <sumanth.yedoti@gmail.com>
2023-04-02 11:40:22 +0530

    Add tests for variable match with string, boolean, null, undefined

--- src/__tests__/variable.test.js
+++ src/__tests__/variable.test.js
@@ -1,10 +1,44 @@
 import matchIf from "../variable"
 
-describe('variable Match', () => {
+describe('variable Match with number', () => {
   test('should return then Value when variables matches', () => {
     expect(matchIf(10).is(10).then(100)).toEqual(100)
+    expect(matchIf(10.34).is(10.34).then(100)).toEqual(100)
   })
   test('should return false when variables does not matche', () => {
     expect(matchIf(1).is(10).then(100)).toEqual(false)
+    expect(matchIf(10.23).is(10.32).then(100)).toEqual(false)
+  })
+});
+
+describe('variable Match with string', () => {
+  test('should return then Value when variables matches', () => {
+    expect(matchIf("Hello").is("Hello").then(100)).toEqual(100)
+    expect(matchIf("my name is Mai Nam").is("my name is Mai Nam").then(100)).toEqual(100)
+  })
+  test('should return false when variables does not matche', () => {
+    expect(matchIf("Hello").is("hello").then(100)).toEqual(false)
+  })
+});
+
+describe('variable Match with boolean', () => {
+  test('should return then Value when variables matches', () => {
+    expect(matchIf(true).is(true).then(100)).toEqual(100)
+    expect(matchIf(false).is(false).then(100)).toEqual(100)
+  })
+  test('should return false when variables does not matche', () => {
+    expect(matchIf(true).is(false).then(100)).toEqual(false)
+    expect(matchIf(false).is(true).then(100)).toEqual(false)
+  })
+});
+
+describe('variable Match with null/undefined', () => {
+  test('should return then Value when variables matches', () => {
+    expect(matchIf(null).is(null).then(100)).toEqual(100)
+    expect(matchIf(undefined).is(undefined).then(100)).toEqual(100)
+  })
+  test('should return false when variables does not matche', () => {
+    expect(matchIf(null).is(undefined).then(100)).toEqual(false)
+    expect(matchIf(undefined).is(null).then(100)).toEqual(false)
   })
 });
