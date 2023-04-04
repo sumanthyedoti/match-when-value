a861ce6b0a389c76c36ddb4d7bd7022743dee642
Sumanth Yeodti <sumanth.yedoti@gmail.com>
2023-04-04 22:41:45 +0530

    Refactor tests

--- src/__tests__/match.test.js
+++ src/__tests__/match.test.js
@@ -1,43 +1,22 @@
 import match from "../variable"
 
-describe('variable Match with number', () => {
+describe('variable Match with primitive values', () => {
   test('should return then Value when variables matches', () => {
     expect(match(10).by(10).then(100)).toEqual(100)
     expect(match(10.34).by(10.34).then(100)).toEqual(100)
-  })
-  test('should return false when variables does not matche', () => {
-    expect(match(1).by(10).then(100)).toEqual(false)
-    expect(match(10.23).by(10.32).then(100)).toEqual(false)
-  })
-});
-
-describe('variable Match with string', () => {
-  test('should return then Value when variables matches', () => {
     expect(match("Hello").by("Hello").then(100)).toEqual(100)
     expect(match("my name is Mai Nam").by("my name is Mai Nam").then(100)).toEqual(100)
-  })
-  test('should return false when variables does not matche', () => {
-    expect(match("Hello").by("hello").then(100)).toEqual(false)
-  })
-});
-
-describe('variable Match with boolean', () => {
-  test('should return then Value when variables matches', () => {
     expect(match(true).by(true).then(100)).toEqual(100)
     expect(match(false).by(false).then(100)).toEqual(100)
-  })
-  test('should return false when variables does not matche', () => {
-    expect(match(true).by(false).then(100)).toEqual(false)
-    expect(match(false).by(true).then(100)).toEqual(false)
-  })
-});
-
-describe('variable Match with null/undefined', () => {
-  test('should return then \'by\' value variables matches', () => {
     expect(match(null).by(null).then(100)).toEqual(100)
     expect(match(undefined).by(undefined).then(100)).toEqual(100)
   })
   test('should return false when variables does not matche', () => {
+    expect(match(1).by(10).then(100)).toEqual(false)
+    expect(match(10.23).by(10.32).then(100)).toEqual(false)
+    expect(match("Hello").by("hello").then(100)).toEqual(false)
+    expect(match(true).by(false).then(100)).toEqual(false)
+    expect(match(false).by(true).then(100)).toEqual(false)
     expect(match(null).by(undefined).then(100)).toEqual(false)
     expect(match(undefined).by(null).then(100)).toEqual(false)
   })
